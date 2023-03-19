34ed160533dfe08e42dc43f4e16c955ae322faa3
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-03-19 18:32:07 +0530

    Changes in Main.js and Main.css. Removed Nav.js.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,13 +4,8 @@ import Logo from "../Assets/Logos/Logo.svg"
 function Header() {
     return (
         <>
-            <meta name="description" content="Little Lemon Restaurant is a mediterianian restaurant that serve class italian and mexican food with a liitle bit of an indian touch." />
-            <meta name="og:title" content="Little Lemon" />
-            <meta name="og:description" content="Little Lemon Restaurant is a mediterianian restaurant that serve class italian and mexican food with a liitle bit of an indian touch." />
-            <meta name="og:image" content={Logo}/>
-
             <header>
-                <img src={Logo} alt="logo" width="540px" />
+                <img src={Logo} alt="logo" />
             </header>
         </>
     )
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import image from '../Assets/images/restaurantfood.jpg';
+import "./Main.css";
 
 function Main() {
     return (
         <>
-            <main></main>
+            <main>
+                <div className='left-side'>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p> We are a family owned <br /> Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    <button>Book Online</button>
+                </div>
+
+                <div className="right-side">
+                    <img src={image} alt="heroImage" />
+                </div>
+            </main>
         </>
     )
 }
