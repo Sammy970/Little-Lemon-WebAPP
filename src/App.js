b9d885472a71a78576ca7275b89a71133edc60ae
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-03-20 00:59:25 +0530

    Small Changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Header from "./Components/Header";
 import Homepage from "./Components/Homepage"
 import Specials from "./Components/Specials"
 
 function App() {
   return (
     <>
+      {/* Header + Navbar */}
+      <Header />
+      {/* Hero Page */}
       <Homepage />
+      {/* Specials Page */}
       <Specials />
-
     </>
   );
 }
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Header from "./Header";
 import Hero from "./Hero";
 // import Footer from "./Components/Footer";
 
 function Homepage() {
   return (
     <>
-        {/* Header + Navbar */}
-        <Header/>
-
         {/* Hero Section */}
         <Hero />
     </>
