d6029c894d19467fb611f3270282db88bf7775c6
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-03-19 19:31:14 +0530

    Added Header.js and Header.css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import Logo from "../Assets/Logos/Logo.svg"
+import Logo from '../Assets/Logos/Logo.svg'
+import "./Header.css"
 
 function Header() {
     return (
         <>
             <header>
-                <img src={Logo} alt="logo" />
+                <img className='logoImg' src={Logo} alt="logo" />
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">Reservations</a></li>
+                        <li><a href="#">Order Online</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </nav>
             </header>
         </>
     )
